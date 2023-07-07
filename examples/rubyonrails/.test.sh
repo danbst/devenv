1e32382b32287c9a9e800a985eb2264d75516f39
Domen Kožar <domen@dev.si>
2023-07-07 09:38:25 +0100

    Update examples/rubyonrails/.test.sh
    Co-authored-by: Bob van der Linden <bobvanderlinden@gmail.com>

--- examples/rubyonrails/.test.sh
+++ examples/rubyonrails/.test.sh
@@ -3,7 +3,5 @@ set -ex
 rails new blog -d=postgresql
 devenv up&
 timeout 20 bash -c 'until echo > /dev/tcp/localhost/5100; do sleep 0.5; done'
-pushd blog
-    rails db:create
-popd
+(cd blog && rails db:create)
 curl -s http://localhost:5100/ | grep "version"
